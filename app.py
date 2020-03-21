1bfe6b1ed254aef60d8bf321445187af58a2ef39
Caroline Solis <csolis2@mail.sfsu.edu>
2020-03-21 13:19:56 -0700

    update6

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static/')
 
 
 @app.route('/', methods=['GET'])
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     port = int(5000)
