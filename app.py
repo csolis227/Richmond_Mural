017b2a0e976e8ef0a753160458f8e6fbedb9d894
Caroline Solis <csolis2@mail.sfsu.edu>
2020-03-21 12:19:26 -0700

    update5

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 @app.route('/index')
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(5000)
+    app.run(host='0.0.0.0', port=port, debug=True)
